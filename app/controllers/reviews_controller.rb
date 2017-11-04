c09226dcf8f42a7e0c07f23309c8577c0918e6ed
Rik Lomas <lomas.rik@gmail.com>
2017-11-04 11:37:05 -0400

    added validations to our reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,14 +15,19 @@ class ReviewsController < ApplicationController
   end
   
   def create
-    # take info from the form and add it to the database
+    # take info from the form and add it to the model
     @review = Review.new(form_params)
     
-    # save this to the database
-    @review.save
+    # we want to check if the model can be saved
+    # if it is, we're go the home page again
+    # if it isn't, show the new form
+    if @review.save
+      redirect_to root_path
+    else
+      # show the view for new.html.erb
+      render "new"
+    end
     
-    # redirect back to the home page
-    redirect_to root_path
   end
   
   def show
@@ -52,10 +57,13 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     
     # update with the new info from the form
-    @review.update(form_params)
+    if @review.update(form_params)
     
-    # redirect somewhere new
-    redirect_to review_path(@review)
+      # redirect somewhere new
+      redirect_to review_path(@review)
+    else
+      render "edit"
+    end
   end
   
   def form_params
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  
+  validates :title, presence: true
+  validates :body, length: { minimum: 10 }
+  validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+  
 end
